0b98bb7f18274b73babf02f5c3f91ef4a55e80a3
Mads Harder <mads987a@gmail.com>
2025-11-28 20:57:07 +0100

    chore(fuzzer_central_expansion_and_syntactic_analysis): Fixed class name and cleaned up

--- group30/fuzzer_central_expansion_and_syntactic_analysis/main.py
+++ group30/fuzzer_central_expansion_and_syntactic_analysis/main.py
@@ -12,7 +12,7 @@ import logging
 
 
 
-class CentralExpansionStrategy(Strategy):
+class CentralExpansionAndSyntaticAnalysisStrategy(Strategy):
     def __init__(self, method_signature: jvm.AbsMethodID, argument: None|Input):
         self.method_signature = method_signature
         self.argument = argument # FIXME: is this needed???
@@ -47,38 +47,6 @@ class CentralExpansionStrategy(Strategy):
                     outputs_encountered.add(interpreter.run(self.method_signature, Input(values=generated_input), stop_event))
 
 
-            # while not stop_event.is_set():
-            #     input_val = []
-
-            #     # Generate random inputs based on method signature
-            #     for elem in self.method_signature.extension.params:
-
-            #         match elem:
-            #             case jvm.Int():
-            #                 val = random.randint(-2147483648, 2147483647) # -2^31 to (2^31)-1
-            #                 input_val.append(jvm.Value.int(val))
-            #             case jvm.Boolean():  # boolean
-            #                 val = random.choice([True, False])
-            #                 input_val.append(jvm.Value.boolean(val))
-            #             case jvm.Array(jvm.Int()):
-            #                 length = random.randint(0, 5)
-            #                 array_vals = [random.randint(-2147483648, 2147483647) for _ in range(length)]
-            #                 input_val.append(jvm.Value.array(jvm.Int(), array_vals))
-            #             case jvm.Array(jvm.Char()):
-            #                 length = random.randint(0, 5)
-            #                 array_vals = [chr(random.randint(32, 126)) for _ in range(length)]
-            #                 input_val.append(jvm.Value.array(jvm.Char(), array_vals))
-            #             case _:
-            #                 # FIXME: the thread fails but this error is not propagated back
-            #                 # to the main thread, so we just stop fuzzing here
-            #                 raise NotImplementedError(f"Random input generation not implemented for type: {elem}")
-
-            #     method_input: Input = Input(values=(*input_val,))
-
-            #     outputs_encountered.add(interpreter.run(self.method_signature, method_input, stop_event))
-
-
-
         static_method_inputs = get_static_variables_combinations(self.method_signature)
 
         # Start fuzzing in a separate thread
@@ -102,5 +70,5 @@ class CentralExpansionStrategy(Strategy):
 
 class CentralExpansionAndSyntaticAnalysisFuzzer(Fuzzer):
     def __init__(self, method_signature: jvm.AbsMethodID, argument: None|Input):
-        strategy = CentralExpansionStrategy(method_signature, argument)
+        strategy = CentralExpansionAndSyntaticAnalysisStrategy(method_signature, argument)
         super().__init__(strategy)
